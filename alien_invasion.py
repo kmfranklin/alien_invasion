51749dad1323a7b512ae457521d91d2127fb6693
Kevin Franklin <kevin@franklindesign.io>
2025-03-28 20:16:22 -0400

    Add the first row of aliens to the game

--- alien_invasion.py
+++ alien_invasion.py
@@ -5,6 +5,7 @@ import pygame
 from settings import Settings
 from ship import Ship
 from bullet import Bullet
+from alien import Alien
 
 class AlienInvasion:
   """Overall class to manage game assets and behavior."""
@@ -23,6 +24,9 @@ class AlienInvasion:
 
     self.ship = Ship(self)
     self.bullets = pygame.sprite.Group()
+    self.aliens =  pygame.sprite.Group()
+
+    self._create_fleet()
 
     # Set the background color.
     self.bg_color = (230, 230, 230)
@@ -78,12 +82,32 @@ class AlienInvasion:
       if bullet.rect.bottom <= 0:
         self.bullets.remove(bullet)
 
+  def _create_fleet(self):
+    """Create the fleet of aliens."""
+    # Create an alien and keep adding aliens until there's no room left.
+    # Spacing between aliens is one alien width.
+    alien = Alien(self)
+    alien_width = alien.rect.width
+
+    current_x = alien_width
+    while current_x < (self.settings.screen_width - 2 * alien_width):
+      self._create_alien(current_x)
+      current_x += 2 * alien_width
+
+  def _create_alien(self, x_position):
+    """Create an alien and place it in the row."""
+    new_alien = Alien(self)
+    new_alien.x = x_position
+    new_alien.rect.x = x_position
+    self.aliens.add(new_alien)
+
   def _update_screen(self):
     """Update images on the screen, and flip to the new screen."""
     self.screen.fill(self.settings.bg_color)
     for bullet in self.bullets.sprites():
       bullet.draw_bullet()
     self.ship.blitme()
+    self.aliens.draw(self.screen)
 
     pygame.display.flip()
 
